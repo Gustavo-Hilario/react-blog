8e4cd8e5181d0a38d0c4862f07eddf3ce106a498
Gustavo_H <gustavo.hilario@automattic.com>
2022-09-17 16:12:21 -0300

    253. Making a Request From an Action Creator -- BAD APPROACH

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
-export const fetchPosts = () => {
+import jsonplaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = async () => {
+    // BAD APPROACH
+    /* const response = await jsonplaceholder.get("/posts", {});
+
     return {
         type: "FECTH_POSTS",
-    };
+        payload: response,
+    }; */
 };

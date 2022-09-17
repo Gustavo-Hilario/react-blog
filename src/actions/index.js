302c96771c14cdfb56459feb7290bde8e712fa41
Gustavo_H <gustavo.hilario@automattic.com>
2022-09-17 16:29:15 -0300

    253. Making a Request From an Action Creator -- BAD APPROACH 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 import jsonplaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = async () => {
-    // BAD APPROACH
+    // BAD APPROACH --
+    // Async Await return other values that cause this issue
+    // API requests is also and issues as there is not way to pause the Redux cycle (reducers and others)
     /* const response = await jsonplaceholder.get("/posts", {});
 
     return {

70e952499c7542ec6c263f963653ce94770e172c
Gustavo_H <gustavo.hilario@automattic.com>
2022-09-19 09:52:00 -0300

    279. Multiple API requests for the same userID issue – fixing this with Lodash memoize function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,21 @@
 import jsonplaceholder from "../apis/jsonPlaceholder";
 
+// Importing memoize function from Lodash to make sure fetchUsers won't make multiple requests
+import _ from "lodash";
+
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonplaceholder.get("/posts");
 
     dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+// We cannot memoize any of the functions here as a new request is made every time
+export const fetchUser = (id) => (dispatch) => {
+    _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonplaceholder.get(`/users/${id}`);
 
     dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
